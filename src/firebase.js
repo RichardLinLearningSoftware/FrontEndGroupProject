996f84cfe5adfcec975467fcd5b5505cd80e921d
Richard Lin <1211675@student.roc-nijmegen.nl>
2026-05-27 09:00:37 +0200

    Initial commit yeah
    Just starting

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyiWqrC_Ecyd_B_8eMvhbrOAs3R3dZuP4",
+  authDomain: "socialmediaproject-4d298.firebaseapp.com",
+  projectId: "socialmediaproject-4d298",
+  storageBucket: "socialmediaproject-4d298.firebasestorage.app",
+  messagingSenderId: "977346968764",
+  appId: "1:977346968764:web:8ae00e62414702512164b9"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
---
+++ src/test.jsx
@@ -0,0 +1,51 @@
+import { collection, doc, getDoc, getDocs, getFirestore, where, addDoc, deleteDoc } from "firebase/firestore";
+import { firebaseConfig, app, db } from "./firebase.js";
+/* 
+Check if a document exist
+
+var docRef = db.collection("cities").doc("SF");
+
+docRef.get().then((doc) => {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch((error) => {
+    console.log("Error getting document:", error);
+});
+*/
+
+async function GetSingleData() {
+  let collectionName = document.getElementById("getAllCollectionInput").value;
+  let documentName = document.getElementById("getAllDocumentInput").value;
+  const docSnap = await getDoc(doc(db, collectionName.trim(), documentName.trim()));
+  console.log("Collection name: ",collectionName);
+  console.log(documentName, "=>",docSnap.data());
+  console.log("Name: ",docSnap.data().name);
+  console.log("Description: ",docSnap.data().desc);
+}
+
+async function GetAllData() {
+  let collectionName = document.getElementById("getAllCollectionInput").value;
+  const querySnapshot = await getDocs(collection(db, collectionName.trim()));
+  console.log("Collection name: ",collectionName);
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, "=>",doc.data());
+  });
+}
+
+async function CreateData() {
+    await addDoc(collection(db, "testingStuff"), {
+        name: "Los Angeles",
+        state: "CA",
+        country: "USA"
+    });
+}
+
+async function  DeleteData() {
+    await deleteDoc(doc(db, "testingStuff", "Xpaq1O5dTmHrAzQuDXYQ"));
+}
+
+export { GetAllData, GetSingleData, CreateData, DeleteData };
