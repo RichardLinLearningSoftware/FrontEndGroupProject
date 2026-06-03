45438a814bad9ae9211ee59f738c2f25a71cc65b
Richard Lin <1211675@student.roc-nijmegen.nl>
2026-06-03 11:40:58 +0200

    Login/Register commit
    -You can now create acounts and login

--- src/Pages/Pages.jsx
+++ src/Pages/Pages.jsx
@@ -1,5 +1,25 @@
 import { useEffect, useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { BrowserRouter,  Route, Routes, NavLink, useSearchParams} from 'react-router';
+import { onAuthStateChanged, signOut  } from "firebase/auth";
+import { auth } from "../firebase.js";
+
+const user = auth.currentUser;
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
+    console.log("is logged in");
+    console.log(user.email);
+    // ...
+  } else {
+    console.log("not logged in");
+    // User is signed out
+    // ...
+  }
+});
 
 function HomePage(){
     return(
@@ -20,4 +40,59 @@ function TestPage(){
     );
 }
 
-export {HomePage, ContactPage, TestPage}
+function Register(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function RegisterUser(e) {
+        e.preventDefault();
+        try {
+            const userCredential = await createUserWithEmailAndPassword( auth, email, password);
+            console.log(userCredential.user);
+        } catch (error) {
+            console.log(error.code);
+            console.log(error.message);
+        }
+    }
+    
+    return (
+        <>
+            <h2>Register</h2>
+            <form onSubmit={RegisterUser}>
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email"/>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="password"/>
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    );
+}
+
+function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function LoginUser(e) {
+        e.preventDefault();
+        try {
+            const userCredential = await signInWithEmailAndPassword( auth, email, password);
+            console.log(userCredential.user);
+        } catch (error) {
+            console.log(error.code);
+            console.log(error.message);
+        }
+    }
+    
+    return (
+        <>
+            <h2>Login</h2>
+            <form onSubmit={LoginUser}>
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email"/>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="password"/>
+                <button type="submit">Submit</button>
+            </form>
+            <button onClick={()=>signOut(auth)}>Logout</button>
+        </>
+    );
+}
+
+export {HomePage, ContactPage, TestPage, Register, Login}
